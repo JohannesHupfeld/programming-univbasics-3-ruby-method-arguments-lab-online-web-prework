d506895a60675e9042a73b7a53642cda14414de8
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-09-04 02:26:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,6 @@ end
 def introduction_with_language_optional(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
-def introduction_with_language_optional(name,ruby/language)
-  puts "Hi, my name is #{name} and I am learning to program in #{ruby/language}."
+def introduction_with_language_optional(name,language="ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
